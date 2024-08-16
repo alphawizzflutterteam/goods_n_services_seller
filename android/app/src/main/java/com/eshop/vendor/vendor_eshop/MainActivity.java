168a54a6fe373c2f46993a72fcbb071475a07fde
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-16 13:15:46 +0530

    first commit

---
+++ android/app/src/main/java/com/eshop/vendor/vendor_eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.goods_n_service.seller;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,24 @@
+final String appName = 'Goods N Service Seller';
+
+final String packageName = 'com.goods_n_service.seller';
+
+final String iosPackage = 'com.goods_n_service.seller';
+
+// final String baseUrl = 'https://vendor.eshopweb.store/seller/app/v1/api/';
+
+// final String jwtKey = "68f05dec6014f68e760c5c5fa3e31bcf391a2e10";
+
+// final String baseUrl = 'https://alphawizztest.tk/ZuqZuq1/seller/app/v1/api/';
+// final String baseUrl = 'https://developmentalphawizz.com/goodsnservice/seller/app/v1/api/';
+final String baseUrl = 'https://goodsnservice.com/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
